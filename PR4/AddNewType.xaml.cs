0310e5450c507c150f7d0c0cc8380452c033d857
MrEntr <MrEntr1@gmail.com>
2023-09-29 01:21:50 +0300

    da

---
+++ PR4/AddNewType.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR4
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewType.xaml
+    /// </summary>
+    public partial class AddNewType : Window
+    {
+        public static string newtype;
+        public AddNewType()
+        {
+            InitializeComponent();
+        }
+        private void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            newtype = newType.Text;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
